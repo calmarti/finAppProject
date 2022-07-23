0d3ad3a0338b54d76259e0abaef5e441af57b0c6
Ivan Martinez <ivmartinezc@gmail.com>
2022-07-23 10:15:03 +0200

    árbol de rutas y refactorizacion de AsyncSelect y Stock en Home

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'antd/dist/antd.css'
+import { BrowserRouter } from 'react-router-dom';
 //import "@ant - design/flowchart/dist/index.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
